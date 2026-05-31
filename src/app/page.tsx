3df9883b6716181974921f8f1017d13bee301f52
yug-void-dev <yugsharma2421@gmail.com>
2026-05-31 16:23:46 +0530

    Updated the UI & Added the Fetch Courses Functionality From Supabase Database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,43 @@
-import { motion } from "framer-motion";
+import { Suspense } from "react";
 import Sidebar from "./components/Sidebar";
 import HeroTile from "./components/HeroTile";
 import CourseCard from "./components/CourseCard";
 import ActivityTile from "./components/ActivityTile";
-import { courses } from "../../data/courses";
-export default function Home() {
+import { Course } from "@/types";
+import Loading from "./loading";
+import { supabase } from "@/lib/supabase";
+
+const getCourses = async () => {
+  const { data, error } = await supabase.from("courses").select("*");
+  if (error) {
+    return [];
+  }
+  return data;
+};
+
+
+export default async function Home() {
+  const courses: Course[] = await getCourses();
   return (
-    <main className="min-h-screen bg-black text-white p-6">
-      <div className="max-w-7xl mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-[90px_1fr] gap-6">
+    <div className="flex h-screen bg-[#060606] overflow-hidden">
+      <Sidebar />
 
-          <Sidebar />
-
-          <div className="space-y-6">
+      <main className="flex-1 overflow-y-auto p-6">
+        <Suspense fallback={<Loading />}>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+            {/* Hero tile — full width */}
             <HeroTile />
 
-            <div className="grid md:grid-cols-2 gap-6">
-              {courses.map((course) => (
-                <CourseCard
-                  key={course.id}
-                  title={course.title}
-                  progress={course.progress}
-                  icon={course.icon}
-                />
-              ))}
+            {/* Course tiles */}
+            {courses.length != 0 &&  courses.map((course, index) => (
+              <CourseCard key={course.id} course={course} index={index} />
+            ))}
 
-             <ActivityTile />
-            </div>
+            {/* Activity tile — full width */}
+            <ActivityTile />
           </div>
-
-        </div>
-      </div>
-    </main>
+        </Suspense>
+      </main>
+    </div>
   );
 }
